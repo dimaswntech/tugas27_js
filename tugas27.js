05fbd6a11ceda0139f66b088a484d7dc9323d7ab
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:15:15 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+function tugas(){
+    var a = "No";
+    var b =" rumah saya"
+    var c =30;
+    var d ="A";
+
+    console.log(a+b,String(c)+d);
+}
+tugas();
+function tugas1(){
+    var a = "Jalan pangeran Tirtayasa No";
+    var b =70;
+    console.log(a+String(b));
+}
+tugas1();
